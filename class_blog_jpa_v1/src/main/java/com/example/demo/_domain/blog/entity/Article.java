209c261c90e755588b2347e9a7e3a73181cff66e
wjdgns5 <rhfovkeh5@naver.com>
2024-10-02 14:25:24 +0900

    update- 글 목록 조회 api -5

--- class_blog_jpa_v1/src/main/java/com/example/demo/_domain/blog/entity/Article.java
+++ class_blog_jpa_v1/src/main/java/com/example/demo/_domain/blog/entity/Article.java
@@ -6,14 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 // 반드시 기본 생성자가 있어야 된다.
 @Entity(name = "tb_article")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Article {
 	
 	// 특정 생성자에만 빌더 패턴을 추가할 수 있다.
